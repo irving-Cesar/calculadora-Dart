9e9f984698034251d594cf1f0f5813590e994a17
irving-Cesar <irvingcesar22@gmail.com>
2022-06-04 20:25:06 -0300

    tratamento de dados com try catch

--- Calculadora/Calculadora.dart
+++ Calculadora/Calculadora.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 
 class Operacao {
   
-  adicao(n1, n2) { return n1 + n2; }  
+  adicao(n1, n2) { print(n1 + n2); }  
 
-  divisao(n1, n2) { return n1 / n2; }  
+  divisao(n1, n2) { print(n1 / n2); }  
 
-  multiplicacao(n1, n2) { return n1 * n2; }
+  multiplicacao(n1, n2) { print(n1 * n2); }
 
-  subtracao(n1, n2) { return n1 - n2; } 
+  subtracao(n1, n2) { print(n1 - n2); } 
 
 }
 
@@ -47,19 +47,31 @@ void main() {
   print('Insira o segundo número:');
   String? n2 = stdin.readLineSync()!;
 
-  if (int.parse(n1).isNaN && int.parse(n2).isNaN) {
-    print('Por favor, insira apenas números!');
+  numCheck(numero) {
+    var check = double.parse(numero).toDouble();
 
-  } else {
-    double num1 = double.parse(n1);
-    double num2 = double.parse(n2);
+    if (!check.isNaN) {
+      return true;  
+    } else {
+      throw Exception('Não é um número!');      
+    }
+  }
+  
+  try {
+    if (numCheck(n1) && numCheck(n2)) {
+      double num1 = double.parse(n1);
+      double num2 = double.parse(n2);
 
-    print('Qual a operação você deseja realizar?\nA - Adição\nD - Divisão\nM - Multiplicação\nS - Subtração');
-    String? op = stdin.readLineSync()!;
-    op = op.toLowerCase();
-    
-    Calculadora calculadora = new Calculadora(num1, num2, op);
-    print('Resultado = ${calculadora.calcular()}.');
+      print('Qual a operação você deseja realizar?\nA - Adição\nD - Divisão\nM - Multiplicação\nS - Subtração');
+      String? op = stdin.readLineSync()!;
+      op = op.toLowerCase();
+      
+      Calculadora calculadora = new Calculadora(num1, num2, op);
+      print('Resultado = ${calculadora.calcular()}.');
+    }
+
+  } catch (e){
+    print('Insira apenas números!');
   }
 
 }
