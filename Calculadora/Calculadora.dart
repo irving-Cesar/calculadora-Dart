48be68f12c8f291aceb17bf84f7090a56d1e9ebb
irving-Cesar <irvingcesar22@gmail.com>
2022-06-04 17:28:43 -0300

    second commit

---
+++ Calculadora/Calculadora.dart
@@ -0,0 +1,24 @@
+class Calculadora {
+  double num1 = 0;
+  double num2 = 0;
+
+  // String op =
+
+}
+
+class Operacao {
+
+  subtracao(){} 
+
+  adicao(){}  
+
+  divisao(){}  
+
+  multiplicacao(){}
+
+}
+
+void main() {
+  Calculadora calculadora = new Calculadora();
+  calculadora;
+}
