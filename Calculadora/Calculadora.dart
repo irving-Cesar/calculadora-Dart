5b1fe3913ffb53d084ac36d4483dbc5e52d99371
irving-Cesar <irvingcesar22@gmail.com>
2022-06-04 17:59:15 -0300

    add op

--- Calculadora/Calculadora.dart
+++ Calculadora/Calculadora.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 class Calculadora {
-  Calculadora(double num1, double num2);
+  Calculadora(double num1, double num2, String op);
 
   // String op =
 
@@ -20,11 +20,16 @@ class Operacao {
 }
 
 void main() {
+  
   print('Insira o primeiro número: ');
   double num1 = double.parse(stdin.readLineSync()!);
+
   print('Insira agora o segundo número:');
   double num2 = double.parse(stdin.readLineSync()!);
 
-  Calculadora calculadora = new Calculadora(num1, num2);
+  print('Qual a operação você deseja realizar?\nA - Adição\nD - Divisão\nM - Multiplicação\nS - Subtração');
+  String? op = stdin.readLineSync()!;
+
+  Calculadora calculadora = new Calculadora(num1, num2, op);
   calculadora;
 }
